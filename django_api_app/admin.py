384d6f0bab1b46db561ab7d28c9edd1cd6890055
Александр Смолин <smolll123@mail.ru>
2021-10-22 18:12:32 +0300

    register all models in admin.py

--- django_api_app/admin.py
+++ django_api_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
