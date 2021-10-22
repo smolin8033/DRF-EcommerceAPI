70c7bb0a6252521bc1983a3159ef15b4b6a3cb4f
Александр Смолин <smolll123@mail.ru>
2021-10-22 20:24:21 +0300

    writing viewsets for 3 serializer_classes

--- django_api_app/views.py
+++ django_api_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from . import models, serializers
+from rest_framework import viewsets
 
 # Create your views here.
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = models.Book.objects.all()
+    serializer_class = serializers.BookSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
