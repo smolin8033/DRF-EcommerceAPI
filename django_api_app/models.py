bba1ca665fcddc6abc47c78d525a666fd18e898b
Александр Смолин <smolll123@mail.ru>
2021-10-22 17:53:43 +0300

    created 3 model classes for the app

--- django_api_app/models.py
+++ django_api_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    category = models.ForeignKey(Category, related_name='books', on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13)
+    pages = models.IntegerField()
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    description = models.CharField(max_length=500)
+    imageUrl = models.URLField()
+    status = models.BooleanField(default=True)
+    date_created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_created',)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    product_tag = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    imageUrl = models.URLField()
+    status = models.BooleanField(default=True)
+    date_created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_created')
+
+    def __str__(self):
+        return f'{self.product_tag} {self.name}'
