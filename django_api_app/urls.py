2aa04af7ca35f311a29ab71253531d2651f2b89e
Александр Смолин <smolll123@mail.ru>
2021-10-22 20:44:10 +0300

    writing routers and urls for endpoints

--- django_api_app/urls.py
+++ django_api_app/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import routers
+from . import views
+
+
+router = routers.DefaultRouter()
+router.register('categories', views.CategoryViewSet)
+router.register('books', views.BookViewSet)
+router.register('products', views.ProductViewSet)
+
 
 urlpatterns = [
-    #
+    path('', include(router.urls)),
+    path('<int:pk>/', include(router.urls)),
 ]
