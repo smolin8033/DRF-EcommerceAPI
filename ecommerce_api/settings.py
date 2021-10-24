298d7ce0e363be403bce1b1c5ea8f849acb918b4
Александр Смолин <smolll123@mail.ru>
2021-10-24 22:51:06 +0300

    fixing typo

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.auth_token',
+    'rest_framework.authtoken',
     'django_api_app',
 ]
 
