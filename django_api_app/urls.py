5d33d0146dcfe42581864cf34f2d775735936dbd
Александр Смолин <smolll123@mail.ru>
2021-10-25 01:25:17 +0300

    add endpoint to obtain_auth_token view, generating auth token for users

--- django_api_app/urls.py
+++ django_api_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include, re_path
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 
@@ -12,4 +13,5 @@ router.register('products', views.ProductViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('<int:pk>/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token),
 ]
