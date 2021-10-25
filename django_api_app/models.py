0e273d924905e5ba84414b9f9e8877941f32df14
Александр Смолин <smolll123@mail.ru>
2021-10-25 23:34:01 +0300

    written Cart class

--- django_api_app/models.py
+++ django_api_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -48,3 +49,20 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.product_tag} {self.name}'
+
+
+class Cart(models.Model):
+    cart_id = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    books = models.ManyToManyField(Book)
+    products = models.ManyToManyField(Product)
+
+    class Meta:
+        ordering = ('cart_id', '-created_at')
+
+    def __str__(self):
+        return self.cart_id
