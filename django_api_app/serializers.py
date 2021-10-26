29f6ca9c44d313059d22b8f30b735f86576f0d1b
Александр Смолин <smolll123@mail.ru>
2021-10-26 20:44:19 +0300

    RegisterSerializer written

--- django_api_app/serializers.py
+++ django_api_app/serializers.py
@@ -3,6 +3,35 @@ from django.contrib.auth.models import User
 from . import models
 
 
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+            'email',
+        ]
+        write_only_fields = ['password']
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username = validated_data.get('username', None),
+            email = validated_data.get('email', None),
+            first_name = validated_data.get('first_name', None),
+            last_name = validated_data.get('last_name', None)
+        )
+        if User.objects.filter(email=user.email).exists():
+            raise serializers.ValidationError('Not unique email')
+        elif User.objects.filter(username=user.username).exists():
+            raise serializers.ValidationError('Not unique username')
+        else:
+            user.set_password(validated_data.get('password'))
+            user.save()
+            return user
+
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
