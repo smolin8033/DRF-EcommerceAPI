8c478a580cbc9c81c31d56767768e516252887ab
Александр Смолин <smolll123@mail.ru>
2021-10-26 19:34:34 +0300

    add JWT auth to REST_FRAMEWORK and INSTALLED_APPS

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'django_api_app',
     'corsheaders',
 ]
@@ -134,6 +135,7 @@ REST_FRAMEWORK = {
       'rest_framework.authentication.BasicAuthentication',
       'rest_framework.authentication.SessionAuthentication',
       'rest_framework.authentication.TokenAuthentication',
+      'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
