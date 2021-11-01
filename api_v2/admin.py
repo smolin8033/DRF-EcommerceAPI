f0f693fedaf0ac270193b826ea6826d4e22c2f1b
Александр Смолин <smolll123@mail.ru>
2021-11-02 00:00:54 +0300

    add all app's models to admin.py and register them

--- api_v2/admin.py
+++ api_v2/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Cart)
+class CartAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    pass
