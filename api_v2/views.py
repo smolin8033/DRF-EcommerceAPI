467b673374bcdb20152d4012491ba937179ecd8c
Александр Смолин <smolll123@mail.ru>
2021-11-04 19:49:19 +0300

    written 3 viewsets, CartItemViewSet is customized

--- api_v2/views.py
+++ api_v2/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from . import models, serializers
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
+from rest_framework.exceptions import NotAcceptable
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = models.CartItem.objects.all()
+    serializer_class = serializers.CartItemSerializer
+
+    def create(self, request, *args, **kwargs):
+        cart = get_object_or_404(models.Cart, user=User.objects.get(username='brandnewuser'))
+        product = get_object_or_404(models.Product, pk=request.data['product'])
+        quantity = int(request.data['quantity'])
+        current_item = models.CartItem.objects.filter(cart=cart, product=product)
+        if current_item.count() > 0:
+            raise NotAcceptable('You already have this item in your list')
+        elif quantity > product.quantity:
+            raise NotAcceptable("Your order quantity exceeds our shop's stock")
+
+        cart_item = models.CartItem(cart=cart, product=product, quantity=quantity)
+        cart_item.save()
+        serializer = serializers.CartItemSerializer(cart_item)
+
+        total = float(product.price) * float(quantity)
+        cart.total = float(cart.total) + float(total)
+        cart.save()
+
+        product.quantity = int(product.quantity) - quantity
+        product.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        cart_item = self.get_object()
+        cart = get_object_or_404(models.Cart, user=User.objects.get(username='brandnewuser'))
+        product = get_object_or_404(models.Product, pk=request.data['product'])
+        quantity = int(request.data['quantity'])
+        if quantity > product.quantity:
+            raise NotAcceptable("Your order quantity exceeds our shop's stock")
+        elif quantity > cart_item.quantity:
+            product.quantity -= quantity - cart_item.quantity
+            cart.total = float(cart.total) + float(product.price) * float(
+                quantity - cart_item.quantity
+            )
+        elif quantity < cart_item.quantity:
+            product.quantity += cart_item.quantity - quantity
+            cart.total = float(cart.total) -  float(product.price) * float(
+                cart_item.quantity - quantity
+            )
+        product.save()
+        cart.save()
+
+        serializer = serializers.CartUpdateSerializer(cart_item, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        cart_item = self.get_object()
+        cart = get_object_or_404(models.Cart, user=User.objects.get(username='brandnewuser'))
+        product = get_object_or_404(models.Product, pk=cart_item.product.id)
+        product.quantity += cart_item.quantity
+        cart.total = float(cart.total) - float(product.price) * float(
+            cart_item.quantity
+        )
+        cart_item.delete()
+        product.save()
+        cart.save()
+        return Response(
+            {'detail': ('your item has been deleted.')},
+            status=status.HTTP_204_NO_CONTENT
+        )
