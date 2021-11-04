6c08272b025a64a708ac5921d91a0564ede5bc9c
Александр Смолин <smolll123@mail.ru>
2021-11-04 19:48:23 +0300

    written ModelSerializer-s for all models. 'product' field of CartItemSerializer customized

---
+++ api_v2/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from . import models
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Product
+        fields = '__all__'
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(
+        queryset=models.Product.objects.all(),
+        source = 'product.title'
+    )
+
+    class Meta:
+        model = models.CartItem
+        fields = [
+            'id',
+            'product',
+            'quantity',
+            'cart'
+        ]
+
+
+class CartUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.CartItem
+        fields = [
+            'product',
+            'quantity',
+            'cart'
+        ]
