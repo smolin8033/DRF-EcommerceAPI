ae1ed727e61ebb6e94b73e655eafb7f0bb9223c6
Александр Смолин <smolll123@mail.ru>
2021-11-05 13:49:46 +0300

    provide basename to CartItemViewSet

--- api_v2/urls.py
+++ api_v2/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
-router.register('carts', views.CartItemViewSet)
+router.register('carts', views.CartItemViewSet, basename='carts')
 
 
 urlpatterns = [
